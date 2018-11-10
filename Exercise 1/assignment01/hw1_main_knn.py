2a279ad07ec13a2e6d30652228a33c2ba996a196
Gryllus24 <44650613+Gryllus24@users.noreply.github.com>
2018-11-10 12:49:46 +0100

    main knn Programm korrekte Labelauswahl

--- Exercise 1/assignment01/hw1_main_knn.py
+++ Exercise 1/assignment01/hw1_main_knn.py
@@ -19,21 +19,29 @@ from sklearn.metrics import confusion_matrix
 X_train, y_train = load_mnist('training'  )
 X_test, y_test = load_mnist('testing'   )
 
-
-samind=np.zeros(0)      # collect all the sample numbers
-sample_labels=np.zeros(0)       # saving the labels
+# collect all the sample number
+samind=np.zeros(0)  
+# saving the labels    
+sample_labels=np.zeros(0)       
 
 for i in range(0,10):   # get the 100 training samples from the 10 classes
+    # filter out the indices, where label is i, gives an array with boolean values false and true
+    val= np.isin(y_train,i)
+    # filters the corresponding indices                             
+    ind=np.array(np.where(val))   
+    # make it 1 dimensional                      
+    ind= np.reshape(ind,(ind.shape[1]))                 
+    # randomply sample 100 indices    
+    s=np.random.choice(ind,100 ,replace=False )         
 
-    val= np.isin(y_train,i)                             # filter out the indices, where label is i, gives an array with boolean values false and true
-    ind=np.array(np.where(val))                         # filters the corresponding indices
-    ind= np.reshape(ind,(ind.shape[1]))                 # make it 1 dimensional
-    s=np.random.choice(ind,100 ,replace=False )         # randomply sample 100 indices
-
-    sample_labels=np.concatenate((sample_labels,np.full((100),i)),axis=0)   # save the corresponing labels for easier use later
-    samind=np.concatenate((samind,s),axis=0)            # concatenate new sample results to existing ones
+    # save the corresponing labels for easier use later
+    sample_labels=np.concatenate((sample_labels,np.full((100),i)),axis=0)  
+    # concatenate new sample results to existing ones
+    samind=np.concatenate((samind,s),axis=0)            
 samind=samind.astype(int)
-samples=X_train[samind]                     # the 1000 training samples
+# get the 1000 samples and corresponding labels
+samples=X_train[samind]                    
+samples_y = y_train[samind]
 
   
 
@@ -47,16 +55,18 @@ X_test = np.reshape(X_test, (X_test.shape[0], -1))
 dists =  mlBasics.compute_euclidean_distances(samples,X_test) 
 
 # Calculate the test predictions for k=1 and k=5: 
-y_test_pred_k1 = mlBasics.predict_labels2(dists, y_train,1 ) # prediction for k=1
-
-y_test_pred_k5 = mlBasics.predict_labels2(dists, y_train,5 ) # prediction for k=5
+# prediction for k=1
+y_test_pred_k1 = mlBasics.predict_labels2(dists, samples_y,1 ) 
+# prediction for k=5
+y_test_pred_k5 = mlBasics.predict_labels2(dists,samples_y,5 )
 
 #3) Report results
 # you should get following message '99.91 of test examples classified correctly.'
 print ("Accuracy for k=1: ", '{0:0.02f}'.format(  np.mean(y_test_pred_k1==y_test)*100), "of test examples classified correctly.")
 print ("Accuracy for k=5: ",'{0:0.02f}'.format(  np.mean(y_test_pred_k5==y_test)*100), "of test examples classified correctly.")
 
-cmat_k1=confusion_matrix(y_test,y_test_pred_k1)             # calculate the confusion matrix
+# calculate the confusion matrix
+cmat_k1=confusion_matrix(y_test,y_test_pred_k1)        
 cmat_k5=confusion_matrix(y_test,y_test_pred_k5)
 print("Confusion matrix for k=1: ")
 print(cmat_k1)
