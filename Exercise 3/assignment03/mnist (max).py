44b860b5758e258eaf675f727501e6c290453d2f
theandy94 <andmuel9@uni-bonn.de>
2018-12-12 21:20:04 +0100

    maaaaan........

--- Exercise 3/assignment03/mnist (max).py
+++ Exercise 3/assignment03/mnist (max).py
@@ -1,5 +1,5 @@
 """
-USAGE : To run Nesterov with L2, run 'python 03_opt_reg_MNIST.py -- opt nesterov --reg L2'
+USAGE : To run Nesterov with L2, run 'python 03_opt_reg_MNIST.py --opt nesterov --reg L2'
 """
 
 import argparse
@@ -8,136 +8,140 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-
 # Argument Parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('--opt', default='sgd', help='sgd or nesterov [default: sgd]')
-parser.add_argument('--reg', default= None, help='L1/L2/None [default: None]')
+parser.add_argument('--reg', default=None, help='L1/L2/None [default: None]')
 FLAGS = parser.parse_args()
-
 
 # YOUR CODE HERE : Specify the number of hidden units for each layer
 
-n_input 	= 784 # e.g. MNIST data input (img shape: 28*28)
-n_hidden1	= 800
-n_hidden2	= 800
-n_classes  	= 10 # e.g. MNIST total classes (0-9 digits)
-mnist 		= input_data.read_data_sets('MNIST_data', one_hot=True)
+n_input = 784  # e.g. MNIST data input (img shape: 28*28)
+n_hidden1 = 800
+n_hidden2 = 800
+n_classes = 10  # e.g. MNIST total classes (0-9 digits)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def mlp_config(n_input,n_hidden1,n_hidden2,n_classes):
-# tf Graph variables
-	x = tf.placeholder("float", [None, n_input], name='x')
-	y = tf.placeholder("float", [None, n_classes], name='y')
 
-	# Store layers weight & bias
-	stddev = 0.1 # <== This greatly affects accuracy!! 
-	weights = {
-	    'h1':  tf.Variable(tf.random_normal([n_input, n_hidden1], stddev=stddev)),
-	    'h2':  tf.Variable(tf.random_normal([n_hidden1, n_hidden2], stddev=stddev)),
-	    'out': tf.Variable(tf.random_normal([n_hidden2, n_classes], stddev=stddev))
-	}
-	biases = {
-	    'b1':  tf.Variable(tf.random_normal([n_hidden1])),
-	    'b2':  tf.Variable(tf.random_normal([n_hidden2])),
-	    'out': tf.Variable(tf.random_normal([n_classes]))
-	}
-	return x,y,weights,biases
+def mlp_config(n_input, n_hidden1, n_hidden2, n_classes):
+    # tf Graph variables
+    x = tf.placeholder("float", [None, n_input], name='x')
+    y = tf.placeholder("float", [None, n_classes], name='y')
 
-def mlp_model(x,y,weights,biases):
+    # Store layers weight & bias
+    stddev = 0.1  # <== This greatly affects accuracy!!
+    weights = {
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden1], stddev=stddev)),
+        'h2': tf.Variable(tf.random_normal([n_hidden1, n_hidden2], stddev=stddev)),
+        'out': tf.Variable(tf.random_normal([n_hidden2, n_classes], stddev=stddev))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden2])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+    return x, y, weights, biases
 
-	dropped_input = tf.nn.dropout(x,keep_prob=0.8)
 
-	layer_1 = tf.nn.relu(tf.add(tf.matmul(dropped_input, weights['h1']), biases['b1']))    
-	dropped_hidden_1 = tf.nn.dropout(x,keep_prob=0.5)
+def mlp_model(x, y, weights, biases):
+    dropped_input = tf.nn.dropout(x, keep_prob=0.8)
 
-	layer_2 = tf.nn.relu(tf.add(tf.matmul(dropped_hidden_1, weights['h2']), biases['b2']))
-	dropped_hidden_2 = tf.nn.dropout(x,keep_prob=0.5)
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(dropped_input, weights['h1']), biases['b1']))
+    dropped_hidden_1 = tf.nn.dropout(layer_1, keep_prob=0.5)
+
+    layer_2 = tf.nn.relu(tf.add(tf.matmul(dropped_hidden_1, weights['h2']), biases['b2']))
+    dropped_hidden_2 = tf.nn.dropout(layer_2, keep_prob=0.5)
 
     # Output fully connected layer with a neuron for each class
-	out_layer = tf.matmul(dropped_hidden_2, weights['out']) + biases['out']
-	return tf.nn.softmax(out_layer), out_layer
+    out_layer = tf.matmul(dropped_hidden_2, weights['out']) + biases['out']
+    return tf.nn.softmax(out_layer), out_layer
 
-def compute_loss(logits,weights,y,regularizer):
-	#define parameters
-	weight_decay_l1 = 1e-4
-	weight_decay_l2 = 1e-5
-	# Define loss
-	with tf.name_scope('Loss'):
-		cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits,labels=y))
-		if (regularizer == None):
-			loss = cost
-		elif (regularizer == "L2"): 
-			loss = cost + weight_decay_l2*(tf.nn.l2_loss(weights['h1']) + tf.nn.l2_loss(weights['h2']) + tf.nn.l2_loss(weights['out']))
-		elif (regularizer == "L1"): 
-		    loss = cost + weight_decay_l1*(tf.math.abs(weights['h1']) + tf.math.abs(weights['h2']) + tf.math.abs(weights['out']))
-		tf.summary.scalar("loss",loss)
+
+def compute_loss(logits, weights, y, regularizer):
+    # define parameters
+    weight_decay_l1 = 1e-4
+    weight_decay_l2 = 1e-5
+    # Define loss
+    with tf.name_scope('Loss'):
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+        if (regularizer == None):
+            loss = cost
+        elif (regularizer == "L2"):
+            loss = cost + weight_decay_l2 * (
+                    tf.nn.l2_loss(weights['h1']) + tf.nn.l2_loss(weights['h2']) + tf.nn.l2_loss(weights['out']))
+        elif (regularizer == "L1"):
+            loss = cost + weight_decay_l1 * (
+                    tf.math.abs(weights['h1']) + tf.math.abs(weights['h2']) + tf.math.abs(weights['out']))
+        tf.summary.scalar("loss", loss)
         return loss
-	
-def cal_accuracy(pred,y):
-	with tf.name_scope('accuracy'):
-		with tf.name_scope('correct_prediction'):
-			correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-			with tf.name_scope('accuracy'):
-				correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-				accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-				summary = tf.summary.scalar('accuracy', accuracy)
-				return summary, accuracy
 
-def train_network(optimizer='sgd',regularizer=None):
-	# config and build model
-	x,y,weights,biases = mlp_config(n_input,n_hidden1,n_hidden2,n_classes)
-	pred, logits 	   = mlp_model(x,y,weights,biases) # Check comments inside 'mlp_model' function
-  
-	loss = compute_loss(logits,weights,y,regularizer)
-	accuracy = cal_accuracy(pred,y)
-	
-	# You can try out different learning rates but it should be the same for both 
-	# the optimizers for proper comparison.
 
-	if (optimizer == 'sgd'): 
-		learning_rate = 0.01 
-		train_step    = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+def cal_accuracy(pred, y):
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        with tf.name_scope('accuracy'):
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        summary = tf.summary.scalar('accuracy', accuracy)
+        return summary, accuracy
 
-	elif (optimizer == 'nesterov'):
-		momentum      = 0.9
-		learning_rate = 0.01
-		train_step    = tf.train.MomentumOptimizer(learning_rate,momentum,use_nesterov=True).minimize(loss)
-	init = tf.global_variables_initializer()
-	sess = tf.Session()
-	sess.run(init)
-	
-	# Merge all the summaries and write them out to corresponding directories
-	
-	merged 	     = tf.summary.merge_all()
-	train_writer = tf.summary.FileWriter( './train', sess.graph)
-	test_writer  = tf.summary.FileWriter('./test')
-	tf.reset_default_graph() 
 
-	# Report your results for 10000 iterations. If time does not allow, you can also report
-	# results for 5000 iterations.
+def train_network(optimizer='sgd', regularizer=None):
+    # config and build model
+    x, y, weights, biases = mlp_config(n_input, n_hidden1, n_hidden2, n_classes)
+    pred, logits = mlp_model(x, y, weights, biases)  # Check comments inside 'mlp_model' function
 
-	for i in range(10000):
-	    batch = mnist.train.next_batch(1000) # Fetch batch
-        
+    loss = compute_loss(logits, weights, y, regularizer)
+    accuracy = cal_accuracy(pred, y)
+
+    # You can try out different learning rates but it should be the same for both 
+    # the optimizers for proper comparison.
+
+    if optimizer == 'sgd':
+        learning_rate = 0.01
+        train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+    elif optimizer == 'nesterov':
+        momentum = 0.9
+        learning_rate = 0.01
+        train_step = tf.train.MomentumOptimizer(learning_rate, momentum, use_nesterov=True).minimize(loss)
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    # Merge all the summaries and write them out to corresponding directories
+
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('./train', sess.graph)
+    test_writer = tf.summary.FileWriter('./test')
+    tf.reset_default_graph()
+
+    # Report your results for 10000 iterations. If time does not allow, you can also report
+    # results for 5000 iterations.
+
+    for i in range(10000):
+        batch = mnist.train.next_batch(1000)  # Fetch batch
+
         if (i % 100) == 0:  # Record summaries ( loss etc) and test-set accuracy
-            summary, acc = cal_accuracy(pred,y)
+            summary, acc = cal_accuracy(pred, y)
             test_writer.add_summary(summary, i)
             print('Test Accuracy at step %s: %s' % (i, acc))
         else:
-	    	summary,_ = sess.run([merged,train_step], feed_dict={x: batch[0], y: batch[1]})
-	    	train_writer.add_summary(summary, i)
+            summary, _ = sess.run([merged, train_step], feed_dict={x: batch[0], y: batch[1]})
+            train_writer.add_summary(summary, i)
 
-	train_writer.close()
-	test_writer.close()
-	
-	print ("Accuracy using tensor flow is:")
-	print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
-	writer = tf.summary.FileWriter('mlp_model',sess.graph)
-	writer.add_graph(sess.graph)
+    train_writer.close()
+    test_writer.close()
 
-if __name__=="__main__":
-	optimizer   = FLAGS.opt;
-	regularizer = FLAGS.reg;
-	print('Optimizer : %s Regularizer : %s'%(optimizer,regularizer))
-	train_network(optimizer,regularizer)
-	
+    print("Accuracy using tensor flow is:")
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+    writer = tf.summary.FileWriter('mlp_model', sess.graph)
+    writer.add_graph(sess.graph)
+
+
+if __name__ == "__main__":
+    optimizer = FLAGS.opt
+    regularizer = FLAGS.reg
+    print('Optimizer : %s Regularizer : %s' % (optimizer, regularizer))
+    train_network(optimizer, regularizer)
---
+++ Exercise 3/assignment03/mnistClassify MLP 1.py
@@ -0,0 +1,122 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import matplotlib
+
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+mnist = input_data.read_data_sets("model_data/", one_hot=True)
+
+# Parameters
+learning_rate = 0.02
+training_epochs = 50
+batch_size = 100
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 800  # 1st layer number of neurons
+n_hidden_2 = 800  # 2nd layer number of neurons
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# tf Graph input
+X = tf.placeholder("float", [None, n_input])
+Y = tf.placeholder("float", [None, n_classes])
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+
+# Create model
+def multilayer_perceptron(x, dropout_input, dropout_hidden):
+    # Input dropout layer 20%
+    dropout_0 = tf.nn.dropout(x, dropout_input)
+    # Hidden fully connected layer with 800 neurons
+    layer_1 = tf.add(tf.matmul(dropout_0, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # First hidden dropout layer 50%
+    dropout_1 = tf.nn.dropout(layer_1, dropout_hidden)
+    # Hidden fully connected layer with 800 neurons
+    layer_2 = tf.add(tf.matmul(dropout_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Second hidden dropout layer 50%
+    dropout_2 = tf.nn.dropout(layer_2, dropout_hidden)
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.matmul(dropout_2, weights['out']) + biases['out']
+    """ maybe we should implement here already the softmax layer,
+        currently only used when predicting at the bottom of file
+        and when doing the loss operation, not sure if completely
+        correct, but we are using softmax in learning and prediction,
+        so probably alright.
+    """
+    return out_layer
+
+
+# Construct model
+# using dropout placeholders for input and hidden layers
+d_i = tf.placeholder(tf.float32, name="dropout_probability_input")
+d_h = tf.placeholder(tf.float32, name="dropout_probability_hidden")
+logits = multilayer_perceptron(X, d_i, d_h)
+
+""" For this file implement:
+        1: stochastic gradient descent optimizer
+        (not sure if there is a difference to our GD Optimizer)
+    
+    Have fun playing with the parameters :D
+"""
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+
+# Minimize using the loss calculated using squared error and l2_loss for the layers
+train_op = optimizer.minimize(loss_op)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    loss_list = []
+    y_list = []
+
+    # Training cycle
+    """ Change batch size at top of the file.
+    """
+    for epoch in range(training_epochs):
+        avg_loss = 0.
+        total_batch = int(mnist.train.num_examples / batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, loss = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y, d_i: 0.8, d_h: 0.5})
+            # Compute average loss
+            avg_loss += loss / total_batch
+        if epoch % display_step == 0:
+            loss_list.append(avg_loss)
+            y_list.append(epoch)
+            print("Epoch:", '%04d' % (epoch + 1), "loss={:.9f}".format(avg_loss))
+    print("Optimization Finished!")
+
+    # Test model
+    # Apply softmax to logits. Determines the most likely label based on calculated confidence in the output layer
+    pred = tf.nn.softmax(logits)
+    # For all training samples test if the predicted value is equal to the actual label
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    # For the current session or a default session, evaluate the network on given testdata and calculate the accuracy
+    print("Accuracy:", accuracy.eval({X: mnist.test.images, Y: mnist.test.labels, d_i: 1., d_h: 1.}))
+
+    plt.plot(loss_list)
+    plt.show()
